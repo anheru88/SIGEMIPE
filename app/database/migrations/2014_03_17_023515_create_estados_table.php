d5c97fd04bf060b906d26b220960c8af0cec9563
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-17 03:40:18 -0500

    Renombrado la migración de Estados.

--- app/database/migrations/2014_03_17_023515_create_estados_table.php
+++ app/database/migrations/2014_03_17_023515_create_estados_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateEstadoTable extends Migration {
+class CreateEstadosTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateEstadoTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('estado', function(Blueprint $table) {
+		Schema::create('estados', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('nombre');
 			$table->timestamps();
@@ -27,7 +27,7 @@ class CreateEstadoTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('estado');
+		Schema::drop('estados');
 	}
 
 }
