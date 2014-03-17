3df9cf941d244ee4591f28b96b4f81452e752bf5
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-17 03:04:06 -0500

    Seeds de la tabla estado.

---
+++ app/database/seeds/EstadoTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+class EstadoTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		DB::table('estado')->truncate();
+
+		$estado = [
+			['nombre' => 'activo', 'created_at' => new DateTime(), 'updated_at' => new DateTime()], 
+			['nombre' => 'inactivo', 'created_at' => new DateTime(), 'updated_at' => new DateTime()], 
+			['nombre' => 'eliminado', 'created_at' => new DateTime(), 'updated_at' => new DateTime()] 
+		];
+
+		// Uncomment the below to run the seeder
+		DB::table('estado')->insert($estado);
+	}
+
+}
