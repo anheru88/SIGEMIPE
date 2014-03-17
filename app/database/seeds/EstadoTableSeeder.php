50c495a5eb534c7d0fdc22a4d83c79b889ae7544
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-17 17:51:22 -0500

    Modificado el seed de Estado para prevenir fallos.

--- app/database/seeds/EstadoTableSeeder.php
+++ app/database/seeds/EstadoTableSeeder.php
@@ -5,16 +5,20 @@ class EstadoTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		DB::table('estados')->delete();
 
-		$estados = [
-			['nombre' => 'activo', 'created_at' => new DateTime(), 'updated_at' => new DateTime()], 
-			['nombre' => 'inactivo', 'created_at' => new DateTime(), 'updated_at' => new DateTime()], 
-			['nombre' => 'eliminado', 'created_at' => new DateTime(), 'updated_at' => new DateTime()] 
-		];
+		$count = User::all()->count();
 
-		// Uncomment the below to run the seeder
-		DB::table('estados')->insert($estados);
+		if(!($count > 0)){
+
+			$estados = [
+				['nombre' => 'activo', 'created_at' => new DateTime(), 'updated_at' => new DateTime()], 
+				['nombre' => 'inactivo', 'created_at' => new DateTime(), 'updated_at' => new DateTime()], 
+				['nombre' => 'eliminado', 'created_at' => new DateTime(), 'updated_at' => new DateTime()] 
+			];
+
+			// Uncomment the below to run the seeder
+			DB::table('estados')->insert($estados);	
+		}
 	}
 
 }
