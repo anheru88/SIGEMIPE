a9e8e995647da29fdaf0f6c4c2a4a04193b049a1
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-23 17:13:51 -0500

    Agregado el java script, para hacer la edición del área. y corrección de botones de acciones, agregada la revisión de si existen áreas al momento de llamar el blade, existían redundancias.

--- app/views/area/index.blade.php
+++ app/views/area/index.blade.php
@@ -7,6 +7,7 @@
 {{ Asset::add('css/widgets.css') }}
 {{ Asset::add('js/jquery.js') }}
 {{ Asset::add('js/bootstrap.js') }}
+{{ Asset::add('js/bootbox.min.js') }}
 {{ Asset::add('js/custom.js') }}
 
 @stop
@@ -81,7 +82,7 @@ AREA - SIGEMIPE
 		<!-- Page Heading -->
 		<div class="page-head">
 			<h2 class="pull-left">
-				<i class="icon-list-alt"></i> Area
+				<i class="icon-list-alt"></i> Área
 			</h2>
 
 			<!-- Breadcrumb -->
@@ -96,7 +97,7 @@ AREA - SIGEMIPE
 				<!-- Divider -->
 				<span class="divider">/</span>
 
-				<a href="" class="bread-current">Area</a>
+				<a href="" class="bread-current">Área</a>
 
 			</div>
 			<div class="clearfix"></div>
@@ -112,11 +113,10 @@ AREA - SIGEMIPE
 							<!-- Widget Title -->
 							<div class="widget-head">
 								<!-- Tabla -->
-								<div class="pull-left">Areas </div>
+								<div class="pull-left">Áreas </div>
 
 								<div class="pull-right">
-									<button class="btn btn-sm btn-default" data-toggle="modal" data-target="#ModalCrear"> Nueva Area </button>
-									
+									<button class="btn btn-sm btn-default" data-toggle="modal" data-target="#ModalCrear"> Nueva Área </button>
 								</div>
 
 								<div class="clearfix"></div>
@@ -128,31 +128,30 @@ AREA - SIGEMIPE
 										<tr>
 											<th>#</th>
 											<th>Nombre</th>
-											<th>Editar</th>
-											<th>Eliminar</th>
+											<th>Controles</th>
 										</tr>
 									</thead>
 									<tbody>
+										@if(isset($areas))
 										@foreach ($areas as $element)
 										<tr>
 											<td> {{ $element->id }}</td>
 											<td> {{ $element->nombre }}</td>
 											<td>
-												{{ link_to_route('area.edit', 'Editar', array($element->id), array('class' => 'btn btn-warning')) }}
-											</td>
-											<td>
-												{{Form::open(array('method' => 'DELETE', 'route' => array('area.index', $element->id))) }}
-												{{ Form::submit('Eliminar', array('class' => 'btn btn-danger')) }}
-												{{ Form::close()}}
+												<button class="btn btn-warning" data-id="{{ $element->id }}" data-nombre="{{ $element->nombre }}"><i class="icon-pencil" ></i> </button>
+												<button class="btn btn-danger" data-id="{{ $element->id }}"><i class="icon-remove"></i> </button>
 											</td>
 										</tr>
 										@endforeach
+										@endif
 									</tbody>
 								</table>
 								<div class="widget-foot">
+									@if(isset($areas))
 									<div class="pull-right">
 										{{ $areas->links() }}
 									</div>
+									@endif
 									<div class="clearfix"></div>
 								</div>
 							</div>
@@ -170,6 +169,7 @@ AREA - SIGEMIPE
 
 
 @section('modals')
+<!-- Modal de Create -->
 <div class="modal fade" id="ModalCrear" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
@@ -195,8 +195,78 @@ AREA - SIGEMIPE
 		</div>
 	</div>
 </div>
+
+<!-- Modal de Edit -->
+<div class="modal fade" id="ModalEditar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="myModalLabel">Editar Area</h4>
+			</div>
+			{{ Form::model($areas, array('method' => 'PATCH', 'route' => array('area.update'), 'class' => 'form-horizontal')) }}
+			<div class="modal-body">
+				<div class="form-group">
+					<label for="id" class="col-lg-4 control-label">Id</label>
+					<div class="col-lg-8">
+						<input type="text" class="form-control" placeholder="id" name="id" disabled id="id_editar">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="nombre" class="col-lg-4 control-label">Nombre</label>
+					<div class="col-lg-8">
+						<input type="text" class="form-control" placeholder="Nombre del Area" name="nombre" id="nombre_editar">
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="submit" class="btn btn-success">Actualizar</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+			</div>
+			{{ Form::close() }}
+		</div>
+	</div>
+</div>
+
 @stop
+
 
 @section('scripts')
 {{ Asset::js() }}
+<script type="text/javascript">
+	$('.btn-warning').on('click', function(){
+		var id = $(this).data('id');
+		var nombre = $(this).data('nombre');
+		$('#ModalEditar form').attr('action', document.URL+'/'+id);
+		$('#id_editar').val(id);
+		$('#nombre_editar').val(nombre);
+		$('#ModalEditar').modal('show');
+	});
+
+	$('.btn-danger').on('click', function(){
+		var id = $(this).data('id');
+		bootbox.dialog({
+			message: 'Realmente desea eliminar el area con Id :<strong>' + id + '</strong>',
+			buttons: {
+				si : {
+					label : "Confirmar",
+					className :"btn-danger",
+					callback: function(){
+						
+					}
+				},
+				no : {
+					label : "Cancelar",
+					className : "btn-default",
+					callback : function(){
+					}
+				}
+			}
+		});
+		/*bootbox.confirm('Realmente quiere eliminar el area con Id: ' + id,  function(result){
+
+			console.log(result);
+		});*/
+	});
+</script>
 @stop
