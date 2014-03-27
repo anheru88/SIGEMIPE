65925b67c38d8ee78541013269c18c5b3d213253
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-27 01:59:40 -0500

    creado el faker para la carga de imágenes.

--- app/database/seeds/UsuarioTableSeeder.php
+++ app/database/seeds/UsuarioTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Faker\Factory as Faker;
+
 class UsuarioTableSeeder extends Seeder {
 
 	public function run()
@@ -7,9 +9,11 @@ class UsuarioTableSeeder extends Seeder {
 		// Uncomment the below to wipe the table clean before populating
 		DB::table('usuarios')->delete();
 
+		$faker = Faker::create();
+
 		$usuario = array(
-			['username' => 'admin', 'email' => 'admin@hotmail.com', 'password' => Hash::make('admin'), 'rol' => 'Admin', 'activo' => '1', 'estado' => '1', 'created_at' =>  new DateTime(), 'updated_at' =>  new DateTime()],
-			['username' => 'usuario', 'email' => 'user@hotmail.com', 'password' => Hash::make('usuario'), 'rol' => 'Usuario', 'activo' => '1', 'estado' => '1', 'created_at' =>  new DateTime(), 'updated_at' =>  new DateTime()]
+			['username' => 'admin', 'email' => 'admin@hotmail.com', 'password' => Hash::make('admin'), 'imagen' => '/img/app/profile/default.png','rol' => 'Admin', 'activo' => '1', 'estado' => '1', 'created_at' =>  new DateTime(), 'updated_at' =>  new DateTime()],
+			['username' => 'usuario', 'email' => 'user@hotmail.com', 'password' => Hash::make('usuario'), 'imagen' => $faker->imageUrl(100, 100), 'rol' => 'Usuario', 'activo' => '1', 'estado' => '1', 'created_at' =>  new DateTime(), 'updated_at' =>  new DateTime()]
 		);
 
 		// Uncomment the below to run the seeder
