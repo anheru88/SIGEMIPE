184d951d07f59b88874a76a0d897231b09432b80
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-03-29 14:59:17 -0500

    Header, and Base Layout.

---
+++ app/views/layouts/header.blade.php
@@ -0,0 +1,49 @@
+<!-- Header Starts -->
+<header>
+	<!-- Container -->
+	<div class="container">
+		<!-- Row -->
+		<div class="row">
+			<!-- Logo Section -->
+			<div class="col-md-4">
+				<!-- Logo -->
+				<div class="logo">
+					<h1>
+						<a href="#">SIGEMIPE</a>
+					</h1>
+					<p class="meta">Sistema de Gestion Misional Personeria</p>
+				</div>
+				<!-- Ends Logo -->
+			</div>
+			<!-- Ends Logo Section -->
+			<!-- Button Section -->
+			<div class="col-md-4">
+				<!-- Buttons -->
+				<ul class="nav nav-pills">
+					<li class="dropdown dropdown-big">
+						<a href="#">
+							<i class="icon-envelope-alt"></i>
+							Mensajes
+						</a>
+					</li>
+					<li class="dropdown dropdown-big">
+						<a href="#">
+							<i class="icon-envelope-alt"></i>
+							Mensajes
+						</a>
+					</li>
+					<li class="dropdown dropdown-big">
+						<a href="#">
+							<i class="icon-envelope-alt"></i>
+							Mensajes
+						</a>
+					</li>
+				</ul>
+				<!-- Ends Buttons -->
+			</div>
+			<!-- Ends Button Sections -->
+		</div>		
+		<!-- Fin Row -->
+	</div>
+	<!-- Fin Continer -->
+</header>
